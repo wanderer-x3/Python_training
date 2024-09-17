f58e26b56d2fc45e1f169dda18421f5d1ca479c1
Iliya Kulikov <iliya.kulikov@gmail.com>
2024-09-17 22:27:35 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,11 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while i >= 0:
+    if my_list [i] > 0:
+        print(my_list[i])
+        i += 1
+    elif my_list [i] == 0:
+        i += 1
+        continue
+    else:
+        break
