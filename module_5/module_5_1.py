831fd2d3996846d5788a1185000e310b2278dabe
Iliya Kulikov <iliya.kulikov@gmail.com>
2024-10-17 15:21:48 +0300

    Add files via upload

---
+++ module_5/module_5_1.py
@@ -0,0 +1,21 @@
+class House:
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+
+    def go_to(self, new_floor):
+        if new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+                if new_floor == floor:
+                    print(f'"Ваш этаж."')
+        else:
+            print('"Такого этажа не существует."')
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
