455499ce04de7b9905a19add7946eea26e600961
Iliya Kulikov <iliya.kulikov@gmail.com>
2024-12-29 21:36:24 +0300

    Add files via upload

---
+++ module_13/module_13_3.py
@@ -0,0 +1,28 @@
+import asyncio
+import os
+import logging
+
+# Импорты фремворка
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+# Импорт для скрытия Токена
+from dotenv import find_dotenv, load_dotenv
+load_dotenv(find_dotenv())
+
+bot = Bot(token=os.getenv('BOT_TOKEN'),)
+
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+    # print('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def all_massages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+    # print('Введите команду /start, чтобы начать общение.')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
